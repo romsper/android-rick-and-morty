55babde8b0e962803edcac0e09dd5f0ecf9a3eb8
romsper <r.speranskii@gmail.com>
2023-01-17 22:11:50 -0500

    Init commit

---
+++ app/src/main/java/com/romsper/android_rick_and_morty/ui/ComposeActivity.kt
@@ -0,0 +1,43 @@
+package com.romsper.android_rick_and_morty.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.romsper.android_rick_and_morty.ui.ui.theme.AndroidrickandmortyTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidrickandmortyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidrickandmortyTheme {
+        Greeting("Android")
+    }
+}
